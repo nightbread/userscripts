39807b07acb3f37afa1045699f77b2a991eb6f88
nightbread <nightbread@inbox.lv>
2017-01-17 14:50:31 -0500

    Bump version

--- ipt-filter.user.js
+++ ipt-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name iPT filter
 // @namespace http://github.com/nightbread
-// @version 0.3
+// @version 0.4
 // @encoding utf-8
 // @license https://opensource.org/licenses/MIT
 // @homepage https://github.com/nightbread/userscripts
