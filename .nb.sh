9107cc27928bceb7a8d9d9774ef7eaf256355d40
nightbread <nightbread@inbox.lv>
2017-01-17 14:36:13 -0500

    First commit

---
+++ .nb.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export GIT_SSH="$HOME/usr/bin/nb-git-ssh"
+git config user.email nightbread@inbox.lv
+git config user.name nightbread
+git config sendemail.smtpuser ''
+git config sendemail.from ''
+git config user.signingkey 5B17BCA7
+if command -v diskutil >/dev/null 2>&1; then
+    if diskutil info / | fgrep -i case-sensitive; then
+        git config core.ignorecase false
+    else
+        git config core.ignorecase true
+    fi
+fi
