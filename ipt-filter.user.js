f3b5b3020cb22da8e5a867a6e0fb55fac43f971c
nightbread <nightbread@inbox.lv>
2017-02-05 18:28:01 -0500

    Fix syntax error

--- ipt-filter.user.js
+++ ipt-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name iPT filter
 // @namespace http://github.com/nightbread
-// @version 0.0.18
+// @version 0.0.19
 // @encoding utf-8
 // @license https://opensource.org/licenses/MIT
 // @homepage https://github.com/nightbread/userscripts
@@ -18,7 +18,7 @@
 
     const xvidIPTTeam = /XviD-iPT Team/;
     const prefixes = new RegExp('^(' + [
-        'GirlsDo[A-Z]'
+        'GirlsDo[A-Z]',
         'TokyoHot',
         'HelplessTeens',
         'Piss',
