f93802a522cc9486f4c9d7b1ee50f2c7dad291ad
nightbread <nightbread@inbox.lv>
2019-07-07 20:56:28 -0400

    Add domain, lint

--- uneventful.user.js
+++ uneventful.user.js
@@ -1,50 +1,50 @@
 // ==UserScript==
 // @name         Uneventful
 // @namespace    https://microsoft.com/
-// @version      1.3
+// @version      1.3.1
 // @description  Prevent annoying events from being bound.
 // @author       Audreyshake of Reddit
 // @match        *://*/*
 // @grant        none
 // @run-at       document-start
+// @updateURL https://raw.githubusercontent.com/nightbread/userscripts/master/uneventful.user.js
+// @downloadURL https://raw.githubusercontent.com/nightbread/userscripts/master/uneventful.user.js
 // ==/UserScript==
 
-/* Inspired by <http://stackoverflow.com/a/10326899>. */
+// Inspired by <http://stackoverflow.com/a/10326899>.
 
-const pasteEvents = [
-    'select',
-    'selectstart',
-    'selectionchange',
-    'beforecopy',
-    'beforecut',
-    'beforepaste',
-    'copy',
-    'cut',
-    'paste',
-    'contextmenu'
-];
-
-const activityEvents = [
-    'visibilitychange'
-];
+// const pasteEvents = [
+//   'select',
+//   'selectstart',
+//   'selectionchange',
+//   'beforecopy',
+//   'beforecut',
+//   'beforepaste',
+//   'copy',
+//   'cut',
+//   'paste',
+//   'contextmenu'
+// ];
+// const activityEvents = [
+//   'visibilitychange'
+// ];
+// const mundaneEvents = [
+//   'click',
+//   'mousedown',
+//   'load',
+//   'unload',
+//   'DOMContentLoaded',
+//   'getData',
+//   'setData'
+// ];
 
 const movementEvents = [
-    'keydown',
-    'keypress',
-    'keyup',
-    'wheel',
-    'mousewheel', // Deprecated but apparently still accepted by Chrome
-    'scroll'
-];
-
-const mundaneEvents = [
-    'click',
-    'mousedown',
-    'load',
-    'unload',
-    'DOMContentLoaded',
-    'getData',
-    'setData'
+  'keydown',
+  'keypress',
+  'keyup',
+  'wheel',
+  'mousewheel', // Deprecated but apparently still accepted by Chrome
+  'scroll'
 ];
 
 /*
@@ -56,80 +56,81 @@ const mundaneEvents = [
  * 'my.domain' to match only 'my.domain'.
  */
 const domainPermissions = {
-    '.dailystormer.name': {
-        deny: movementEvents
-    },
-    DEFAULT: {
-        allow: '*'
-    }
+  '.dailystormer.name': {
+    deny: movementEvents
+  },
+  '.unicornriot.ninja': {
+    deny: movementEvents
+  },
+  DEFAULT: {
+    allow: '*'
+  }
 };
 
 // Sites that do not like overriding addEventListener to be read-only
 const aelBlacklist = ['.icloud.com'];
 
 /**
- * @param string domain
- * @param string event
- * @returns bool
+ * @param {string} domain
+ * @param {string} event
+ * @return {boolean}
  */
 function doesCanEvent(domain, event) {
-    let last = domain;
-    let next = last.replace(/..*?\./, '.');
-    let superdomains = [domain, '.' + domain];
+  let last = domain;
+  let next = last.replace(/..*?\./, '.');
+  let superdomains = [domain, '.' + domain];
 
-    for (; next != last; last = next) {
-        superdomains.push(next);
-        next = last.replace(/..*?\./, '.');
+  for (; next != last; last = next) {
+    superdomains.push(next);
+    next = last.replace(/..*?\./, '.');
+  }
+
+  superdomains.push('DEFAULT');
+
+  /**
+   * @param {Object<string, any>} obj
+   * @param {string} key
+   * @return {boolean}
+   */
+  const hop = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
+
+  for (domain of superdomains) {
+    if (hop(domainPermissions, domain)) {
+      const dp = domainPermissions[domain];
+      if (hop(dp, 'deny') &&
+        (dp.deny.indexOf(event) != -1 || dp.deny.indexOf('*') != -1)) {
+        return false;
+      }
+      if (hop(dp, 'allow') &&
+        (dp.allow.indexOf(event) != -1 || dp.allow.indexOf('*') != -1)) {
+        return true;
+      }
     }
+  }
 
-    superdomains.push('DEFAULT');
-
-    for (domain of superdomains) {
-        if (domainPermissions.hasOwnProperty(domain)) {
-            var dp = domainPermissions[domain];
-            if (dp.hasOwnProperty('deny') &&
-                (dp.deny.indexOf(event) != -1 || dp.deny.indexOf('*') != -1)) {
-                return false;
-            }
-            if (dp.hasOwnProperty('allow') &&
-                (dp.allow.indexOf(event) != -1 ||
-                 dp.allow.indexOf('*') != -1)) {
-                return true;
-            }
-        }
-    }
-
-    return false;
+  return false;
 }
 
-[HTMLElement.prototype, document, window].forEach(function(element) {
-    const trueBlueAddEventListener = element.addEventListener;
+[HTMLElement.prototype, document, window].forEach((element) => {
+  const trueBlueAddEventListener = element.addEventListener;
 
-    element.addEventListener = function(type, listener) {
-        if (doesCanEvent(window.location.host, type)) {
-            trueBlueAddEventListener.apply(
-                this, Array.prototype.slice.apply(arguments));
-            // console.groupCollapsed('%cAccepted event: %s', 'color:#aaa', type);
-        } else {
-            console.groupCollapsed(
-                '%cEvent refused: %c%s', 'color:red', 'color:default', type);
-        }
-        // console.log('Element: %O', this);
-        // console.log('Listener: %o', listener);
-        // console.log('Listener source: %s', listener.toString()); // For dealing with really bizarre things?
-        // console.trace();
-        console.groupEnd();
-    };
-
-    /*
-     * Keep scripts from redefining addEventListener. Since we've already captured the real addEventListener, it isn't
-     * really necessary, but I'm not a fan of sites doing it.
-     *
-     * See <http://stackoverflow.com/a/7757493>.
-     */
-    const domain = window.location.host.replace(/..*?\./, '.');
-    if (!aelBlacklist.includes(domain)) {
-        Object.defineProperty(element, 'addEventListener', {
-            value: element.addEventListener, writable: false });
+  element.addEventListener = (type, listener, options) => {
+    if (doesCanEvent(window.location.host, type)) {
+      trueBlueAddEventListener.call(this, type, listener, options);
     }
+  };
+
+  /*
+   * Keep scripts from redefining addEventListener. Since we've already
+   * captured the real addEventListener, it isn't really necessary, but I'm
+   * not a fan of sites doing it.
+   *
+   * See <http://stackoverflow.com/a/7757493>.
+   */
+  const domain = window.location.host.replace(/..*?\./, '.');
+  if (!aelBlacklist.includes(domain)) {
+    Object.defineProperty(element, 'addEventListener', {
+      value: element.addEventListener, writable: false
+    });
+  }
 });
