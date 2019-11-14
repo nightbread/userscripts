4691397c761ad0e52005ea03a6694644f52c9213
nightbread <nightbread@inbox.lv>
2019-11-14 00:18:21 -0500

    Lint

--- .nb.sh
+++ .nb.sh
@@ -6,7 +6,7 @@ git config sendemail.smtpuser ''
 git config sendemail.from ''
 git config commit.gpgsign false
 if command -v diskutil >/dev/null 2>&1; then
-    if diskutil info / | fgrep -i case-sensitive; then
+    if diskutil info / | grep -F -i case-sensitive; then
         git config core.ignorecase false
     else
         git config core.ignorecase true
