8163d1d1a822244188de46a5879dc8807bee5e67
nightbread <nightbread@inbox.lv>
2020-09-06 03:45:18 -0400

    .nb: exit if no GIT_SSH

--- .nb.sh
+++ .nb.sh
@@ -2,13 +2,14 @@
 export GIT_SSH="$HOME/.local/bin/nb-git-ssh"
 if ! [ -f "$GIT_SSH" ]; then
     echo 'Fix missing $GIT_SSH!' >&2
+    exit 1
 fi
 git config user.email nightbread@inbox.lv
 git config user.name nightbread
 git config sendemail.smtpuser ''
 git config sendemail.from ''
 git config commit.gpgsign false
-if command -v diskutil >/dev/null 2>&1; then
+if command -v diskutil > /dev/null 2>&1; then
     if diskutil info / | grep -F -i case-sensitive; then
         git config core.ignorecase false
     else
