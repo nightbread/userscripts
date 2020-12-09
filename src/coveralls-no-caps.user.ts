cde2aaaa1b1eec7906b9cc1ad084d2cde9fc6029
nightbread <nightbread@inbox.lv>
2020-12-09 03:14:36 -0500

    coveralls-no-caps.user: adjust

--- src/coveralls-no-caps.user.ts
+++ src/coveralls-no-caps.user.ts
@@ -13,24 +13,22 @@
 // @namespace    https://www.theguardian.com/
 // ==/UserScript==
 // From https://davidwalsh.name/add-rules-stylesheets
-const coverallsNoCapsStyle = document.createElement('style');
-coverallsNoCapsStyle.appendChild(document.createTextNode('')); // WebKit hack :(
-document.head.appendChild(coverallsNoCapsStyle);
-if (coverallsNoCapsStyle.sheet) {
-  coverallsNoCapsStyle.sheet.insertRule(
-    '* { text-transform: none !important; }',
-    0,
-  );
-}
-for (const el of Array.from(
-  document.querySelectorAll<HTMLAnchorElement>('a[href="/careers"]'),
-)) {
+
+((element, doc, head) =>
+  head.appendChild((element.appendChild(doc.createTextNode('')), element)) &&
+  element.sheet &&
+  element.sheet.insertRule('* { text-transform: none !important; }', 0))(
+  document.createElement('style'),
+  document,
+  document.head,
+);
+const uppercaseFirst = (x: string) => `${x[0]}${x.toLowerCase().substring(1)}`;
+const $$ = document.querySelectorAll.bind(document);
+for (const el of Array.from($$<HTMLAnchorElement>('a[href="/careers"]'))) {
   el.innerHTML = el.innerHTML.replace('CAREERS', 'Careers');
 }
-for (const el of Array.from(
-  document.querySelectorAll<HTMLAnchorElement>('footer a'),
-).filter(el => el.innerText.toUpperCase() == el.innerText)) {
-  el.innerText = `${el.innerText[0]}${el.innerText
-    .toLowerCase()
-    .substring(1)}`;
+for (const el of Array.from($$<HTMLAnchorElement>('footer a')).filter(
+  el => el.innerText.toUpperCase() === el.innerText,
+)) {
+  el.innerText = uppercaseFirst(el.innerText);
 }
