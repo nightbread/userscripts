9ff2318f20fc7aeaa3760f721110fb10009b8f90
nightbread <nightbread@inbox.lv>
2020-12-08 22:39:09 -0500

    code-font.user: single expr

--- src/code-font.user.ts
+++ src/code-font.user.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Monospace for code
-// @version      0.0.4
+// @version      0.0.5
 // @description  Use plain monospace for code on GitHub and other sites.
 // @match        https://github.com/*
 // @match        https://gist.github.com/*
@@ -14,12 +14,10 @@
 // @supportURL   https://www.theguardian.com/
 // @namespace    https://www.theguardian.com/
 // ==/UserScript==
-
-const codeFontStyle = document.createElement('style');
-codeFontStyle.appendChild(document.createTextNode('')); // WebKit hack :(
-document.head.appendChild(codeFontStyle);
-if (codeFontStyle.sheet) {
-  for (const selector of [
+((element, doc, head) =>
+  head.appendChild((element.appendChild(doc.createTextNode('')), element)) &&
+  element.sheet &&
+  [
     // GitHub
     '.blob-code-inner, .blob-num',
     // GitLab
@@ -27,10 +25,9 @@ if (codeFontStyle.sheet) {
     // BitBucket
     '.view-lines',
     '.line-numbers > a',
-  ]) {
-    codeFontStyle.sheet.insertRule(
+  ].forEach(selector =>
+    element.sheet?.insertRule(
       `${selector} { font-family: monospace !important; }`,
       0,
-    );
-  }
-}
+    ),
+  ))(document.createElement('style'), document, document.head);
