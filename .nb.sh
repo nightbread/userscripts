e9e47e2ef03348da76a6c1d12faeac5786bdb400
nightbread <nightbread@inbox.lv>
2021-06-29 13:20:05 -0400

    adjust nb.sh

--- .nb.sh
+++ .nb.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
-export GIT_SSH="${DIR}/.nb-git-ssh"
-if ! [ -f "$GIT_SSH" ] || ! [ -f ~/.ssh/nb-id_rsa ]; then
+if ! [ -f ~/.ssh/nb-id_rsa ]; then
     # shellcheck disable=SC2016
-    echo 'Fix missing $GIT_SSH or missing ~/.ssh/nb-id_rsa!' >&2
+    echo 'Fix missing ~/.ssh/nb-id_rsa!' >&2
     exit 1
 fi
+git config core.sshCommand \
+    "ssh -o ControlMaster=no -o ControlPath=no -o StrictHostKeyChecking=no -o IdentityFile=${HOME}/.ssh/nb-id_rsa"
 git config user.email nightbread@inbox.lv
 git config user.name nightbread
-git config sendemail.smtpuser ''
-git config sendemail.from ''
+git config --unset sendemail.smtpuser
+git config --unset sendemail.from ''
 git config commit.gpgsign false
-if command -v diskutil > /dev/null 2>&1; then
-    if diskutil info / | grep -F -i case-sensitive; then
-        git config core.ignorecase false
-    else
-        git config core.ignorecase true
-    fi
+if command -v diskutil > /dev/null 2>&1 && diskutil info / | grep -qFi case-sensitive; then
+    git config core.ignorecase false
+else
+    git config core.ignorecase true
 fi
