eaebfca21df3f21754f4dde25ccc4b8a6cdbbd29
nightbread <nightbread@inbox.lv>
2023-05-13 00:02:52 -0400

    Add github-disable-codeview-textareas

---
+++ src/github-disable-codeview-textareas.user.ts
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @author       Duologic
+// @description  Disable textareas in the new GitHub code view.
+// @match        https://*.github.com/*
+// @name         Disable codeview textarea
+// @run-at       document-end
+// @version      0.0.1
+// ==/UserScript==
+
+((selector: string): Promise<HTMLTextAreaElement> =>
+  new Promise(resolve => {
+    const el = document.querySelector<HTMLTextAreaElement>(selector);
+    if (el) {
+      return resolve(el);
+    }
+    const observer = new MutationObserver(() => {
+      const el = document.querySelector<HTMLTextAreaElement>(selector);
+      if (el) {
+        resolve(el);
+        observer.disconnect();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }))('#read-only-cursor-text-area')
+  .then(elm => (elm.disabled = true))
+  .catch(console.error);
